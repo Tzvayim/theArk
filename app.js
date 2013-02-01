ae87bd81c87eadbcf218f3bca131a826123bf42e
James Gosnell <jamesgosnell@gmail.com>
2013-01-31 18:11:33 -0600

    ADDED configuration file

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ var express = require('express')
   , passport = require('passport')
   , twitterStrat = require('passport-twitter').Strategy
   , nano = require('nano')
-  , path = require('path');
+  , path = require('path')
+	, config = require('./config')[process.env.NODE_ENV || 'production']
+	;
 
 var app = express();
 
-var TWITTER_CONSUMER_KEY = "";
-var TWITTER_CONSUMER_SECRET = "";
-
 passport.use(new twitterStrat({
-    consumerKey: TWITTER_CONSUMER_KEY,
-    consumerSecret: TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://theark.info/auth/twitter/callback"
+    consumerKey: config.TWITTER_CONSUMER_KEY,
+    consumerSecret: config.TWITTER_CONSUMER_SECRET,
+    callbackURL: config.callbackURL
   },
   function(token, tokenSecret, profile, done) {
     process.nextTick(function () {
@@ -46,19 +45,42 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret:''}));
+  app.use(express.session({ secret:'keyboard cat'}));
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.configure('production', function(){
+  nano = nano('http://nodejitsudb7653225711.iriscouch.com:5984/theark');
+});
+
 app.configure('development', function(){
   app.use(express.errorHandler());
   app.locals.pretty = true;
+  nano = nano('http://localhost:5984/theark');
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/')
+};
+
+function isExistingUser(req, res, next) {
+  //if (){};
+  db.head('rabbit', function(err, _, headers) {
+    if (!err)
+    console.log(headers);
+  });
+  res.redirect('/profile')
+};
+
 app.get('/', routes.index);
+
+app.get('/profile', ensureAuthenticated, function(req, res) {
+  res.render("profile", { user: req.user, title: 'The Ark', subtitle: 'Bnei Noach Database' });
+});
  
 app.get('/auth/twitter',
   passport.authenticate('twitter'),
@@ -81,11 +103,6 @@ app.get('/logout', function(req, res) {
     res.redirect('/');
   }
 );
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/')
-};
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ config.js
@@ -0,0 +1,16 @@
+// fill out with your own keys
+
+var config = {
+	'development': {
+		TWITTER_CONSUMER_KEY: "",
+		TWITTER_CONSUMER_SECRET: "",
+		callbackURL: ""
+	},
+	'production': {
+		TWITTER_CONSUMER_KEY: "",
+		TWITTER_CONSUMER_SECRET: "",
+		callbackURL: ""
+	}
+}
+
+module.exports = config
